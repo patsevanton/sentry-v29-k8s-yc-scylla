d357eb24ed23c55a529fec39554c674ad4eab378
Anton Patsev <patsev.anton@gmail.com>
2026-02-20 12:05:22 +0600

    Keep only Terraform for k8s
    Remove postgres, redis, clickhouse, s3, templatefile.
    Slim versions.tf providers.

--- versions.tf
+++ versions.tf
@@ -8,17 +8,9 @@ terraform {
       source  = "hashicorp/helm"
       version = ">= 3.0.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = ">= 3.0.0"
-    }
     time = {
       source  = "hashicorp/time"
       version = ">= 0.9.0"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = ">= 2.0.0"
     }
     null = {
       source  = "hashicorp/null"
